4247c0f5c590025b3f973298dfa45684dea14049
sofiane <sofiane.kirati@hotmail.com>
2017-05-27 23:53:05 +0200

    make it better

--- server.js
+++ server.js
@@ -7,18 +7,17 @@ app.set('port',(process.env.PORT || 5000));
 var server = require('http').Server(app); 
 var io = require('socket.io')(server); 
 
+
 var clientsID = 0;
-var ballsID = 0;
 var brushPositions = {};
 var map = [];
 var ballPositions = [];
-var sourisPosition = {};
 var clients={};
 
 var a_supprimer = -1;
 var client_concerner = -1;
 //------------------------------------------------------------------------------
-
+// boucle pour remplir le tableau map avec positions des obstacles
 for(var i=0; i<600 ; i+=30){
 	map.push({x:0, y:i, width:30, height:30});
 }
@@ -37,7 +36,7 @@ map.push({x:240, y:270, width:30, height:30});
 map.push({x:240, y:300, width:30, height:30});
 
 //-----------------------------------------------------------------------------
-
+// méthode pour gérer tout type de collision
 function collide(obj1, obj2){
 	return obj1.x + obj1.width > obj2.x && 
 	obj2.x + obj2.width > obj1.x && 
@@ -46,7 +45,7 @@ function collide(obj1, obj2){
 }
 
 //----------------------------------------------------------------------------
-
+// méthode pour calculer le vecteur élémentaire qui sert à déterminer la direction de la balle 
 function tracerBall(data){
 	var pX = data.x;
 	var pY = data.y;
@@ -60,7 +59,7 @@ function tracerBall(data){
 	var rY = y/l;
 
 	return [rX, rY];
-	console.log(rX+"  "+rY);
+
 }
 
 //----------------------------------------------------------------------------
@@ -68,12 +67,21 @@ io.on('connection', function (socket) {
 	console.log("serveur> Client "+clientsID+" s'est connecte");
 	var clientId = clientsID;
 	var nbrpoints = 0;
+	var collision = true;
 	// génération d'une position aléatoire du joueur qui vient de ce connecter 
-	var xAlea = Math.random() * (770 - 35) + 35;
-	var yAlea = Math.random() * (570 - 35) + 35;	
-	
-	clients[clientId] = {clientId: clientId, nbrPoints:nbrpoints};
-	brushPositions[clientId] = {x:xAlea, y:yAlea};
+	while(collision){ // tester si la position générer n'est pas en collision avec les obstacles
+		var xAlea = Math.random() * (770 - 30) + 30;
+		var yAlea = Math.random() * (570 - 30) + 30;
+		var teste_position = {x: xAlea, y: yAlea, width: 32, height: 32};
+		for(var i in map){	
+			collision = collide(teste_position,map[i]);
+			if(collision){
+				break;						   
+			}
+		}	
+	}
+	clients[clientId] = {clientId: clientId, nbrPoints:nbrpoints}; 
+	brushPositions[clientId] = {x: xAlea, y: yAlea};
 
 	// envoyer au client ça position et son ID
 	socket.emit('hello', {id: clientsID, x:brushPositions[clientId].x, y:brushPositions[clientId].y, type:"initialisation"});
@@ -98,7 +106,7 @@ io.on('connection', function (socket) {
 			bX =  data.x+vectElem[0]*vitesse;
 			bY = data.y+vectElem[1]*vitesse;
 			vitesse = vitesse+5;
-			
+				
 			if(client_concerner == clientId ){
 				a_supprimer = -1;
 				client_concerner = -1;
@@ -108,7 +116,6 @@ io.on('connection', function (socket) {
 			else{
 				//remplir le dictionnaire (ballPositions) qui contiendra toutes les balles
 				if(data.ballID == 0){
-					console.log(data.ballID);
 					ballPositions[clientId] = [];
 					ballPositions[clientId].push({ballID: data.ballID, x: bX, y: bY, width: 16, height: 16});
 				}else{
@@ -185,7 +192,6 @@ io.on('connection', function (socket) {
 				if(!coll1 && !coll2 & !coll3){ // si pas de collision
 					io.emit('updateballPosition', {ballPositions: ballPositions,type:"uBallP",clients:clients});
 				}else if(coll1 || coll2 || coll3){ // si y a une des collisions
-					console.log("ya u collision ball ball"+coll3+"         client>>>>>>>><"+clientId);
 					if(coll1){
 						nbrpoints = nbrpoints+1;
 					}
@@ -194,14 +200,13 @@ io.on('connection', function (socket) {
 					clearInterval(myvar);
 				}
 			}		
-		},80);
+		},15);
 	});     
 
         //-----------------------------------------    
 	//lorsqu'un joueur tape une touche 
 	socket.on('keyupdown', function (data) { 
-  	console.log("une touche à été tapé");
-
+  
 	var coll1 = false;
 	var coll2 = false;
 	var id = data.id;
@@ -322,14 +327,6 @@ io.on('connection', function (socket) {
 	});
 }); 
 
-/*server.listen(3000, function() { 
- console.log('Example app listening on port 3000!'); 
-}); */
-
 server.listen(app.get('port'), function() {
 	console.log("Node app running on port",app.get('port'));
 }); 
-
-
-
-
